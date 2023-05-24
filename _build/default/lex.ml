3f00b5022360b4e97ce4e175e66a86880ee1cd4c
Alex Kozik <ajk333@cornell.edu>
2023-05-23 23:19:17 -0400

    Added parsing for strings

--- _build/default/lex.ml
+++ _build/default/lex.ml
@@ -1,4 +1,4 @@
-type token_type = Integer of int | Boolean of bool
+type token_type = Integer of int | Boolean of bool | StringToken of string
 type token = {token_type: token_type; line: int}
 
 
@@ -34,12 +34,25 @@ let int_from_char: char -> int = function
 | _ -> failwith "not an int passed to int_from_char"
 
 
+let string_of_char (c: char) = String.make 1 c
+
+
 
 let rec lex_int (lst: char list) (acc: int): token * char list = match lst with
 | n :: t when is_num n ->
   let n_int: int = int_from_char n in
   lex_int t (acc * 10 + n_int)
 | _ -> ({token_type = (Integer acc); line = 0}, lst)
+
+let rec lex_string (lst: char list) (acc: string): token * char list = match lst with
+| '"' :: t -> ({token_type = StringToken acc; line = 0}, t)
+| '\\':: '"' :: t -> lex_string t (acc ^ "\"")
+| c :: t -> 
+  let char_string: string = string_of_char c in
+
+  lex_string t (acc ^ char_string)
+
+| [] -> failwith "expected closing double quote in lexing string"
 
 
 
@@ -60,12 +73,29 @@ let rec lex (lst: char list): token list =
 
   | n :: _ when is_num n ->
     let int_token, tail = lex_int lst 0 in int_token :: (lex tail)
-  
+
+
+  | '"' :: c :: t ->
+    
+    if c = '"' then 
+      let new_token: token = {token_type = (StringToken ""); line = 0} in
+      new_token :: (lex t)
+  else
+    let new_token, remainder = lex_string (c :: t) "" in
+    new_token :: (lex remainder)
+    
+      
 
   | _ -> failwith "no token matched"
 
 
 
 let string_of_token: token -> string = function
-| {token_type = Boolean b; line = _} -> string_of_bool b
-| {token_type = Integer n; line = _} -> string_of_int n
+| {token_type = Boolean b; line = _} -> let s: string = string_of_bool b in
+
+"<boolean: " ^ s ^ ">"
+
+
+| {token_type = Integer n; line = _} -> let s: string = string_of_int n in
+"<integer: " ^ s ^ ">"
+| {token_type = StringToken s; line = _} -> "<string: " ^ s ^ ">"
