d4f9da9c60d477b593d35a9964cc4fc57e16a2fb
Alex Kozik <ajk333@cornell.edu>
2023-05-26 01:22:13 -0400

    Added a repl

--- main.ml
+++ main.ml
@@ -5,9 +5,6 @@ open Expr
 
 let () = ignore print_tokens_list
 
-
-
-
 type action = Run of string | REPL
 
 let get_action (): action =
@@ -29,7 +26,21 @@ let get_action (): action =
 
 
 
-let run_repl (): unit = ()
+let rec repl_loop (): unit =
+  let input_string: string = read_line () in
+  let e: expr = input_string |> list_of_string |> lex |> parse_expr |> fst in
+  
+  print_endline "[Printing AST]\n"; e |> string_of_expr |> print_endline; print_endline "\n[End AST]\n";
+
+  repl_loop ()
+
+
+let run_repl (): unit = 
+
+  print_endline "[LambdaScript REPL]\n"; repl_loop ()
+  
+
+
 let run_run (dir: string): unit = 
   let contents: string = Reader.read dir in
   let tokens: token list = contents |> Lex.list_of_string |> Lex.lex in
