fd76af9ba5d0f9412d604910b28266b3e418aa20
Alex Kozik <ajk333@cornell.edu>
2023-12-24 21:58:10 -0500

    Testing

--- test/test.ml
+++ test/test.ml
@@ -952,6 +952,18 @@ fold_right (\x -> \y -> x + y) [1,2,3,4,5,6,7,8,9,10] 0
       "[(1, 4), (1, 5), (1, 6), (2, 4), (2, 5), (2, 6), (3, 4), (3, 5), (3, 6)]"
     );
     ({|[x + y | x <- [1, 2, 3], y <- [4, 5, 6]]|}, "[5, 6, 7, 6, 7, 8, 7, 8, 9]");
+    ( {|
+    bind rec fold_right op lst acc <-
+      switch lst =>
+      | [] -> acc
+      | h :: t -> op h (fold_right op t acc)
+    end
+    in
+
+    fold_right (\x -> \y -> x + y) [1 ... 10] 0
+    
+    |},
+      "55" );
   ]
 
 let fold_type_tests =
