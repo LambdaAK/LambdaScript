873cea076fd9303a763bb6ec9c94b9504528f87d
Alex Kozik <ajk333@cornell.edu>
2024-01-09 00:03:09 -0500

    Documentation for typefixer.ml

--- src/typefixer.ml
+++ src/typefixer.ml
@@ -1,16 +1,26 @@
 open Cexpr
 
+(** Given an element [elm] and [lst], [visited elm lst] returns true if [elm] is
+    in [lst] and false otherwise. *)
 let visited (elm : 'a) (lst : 'a list) = List.exists (fun x -> x = elm) lst
 
+(** Given an element [elm] and a list of replacements [lst],
+    [get_replacement elm] returns the replacement of [elm] specified in [lst] *)
 let rec get_replacement (elm : int) (lst : int list) : int =
   match lst with
   | [] -> failwith "failure in get_replacement"
   | h :: _ when h = elm -> 1 (* the first element gets replaced by 1 *)
   | _ :: t -> 1 + get_replacement elm t (* add 1 *)
 
+(** Given a type [t], [fix t] is the type [t] but with type variables replaced
+    in a way such that, when converted to a string, a type variable is less than
+    or equal to all types to the right of it. *)
 let fix (t : c_type) : c_type =
   (* replace written type vars with type vars *)
   let order : int list ref = ref [] in
+  (* [search t] is a helper function that searches through [t] and adds all type
+     variables to [order] in the order that they appear in [t] from left to
+     right. *)
   let rec search (t : c_type) : unit =
     match t with
     | IntType -> ()
